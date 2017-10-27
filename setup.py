28ed7abf5495d20499c150a1d4684042d2d7935e
Wei Ning <nwcgnw@gmail.com>
2017-10-28 01:27:21 +1300

    use tox (pytest) to run tests

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+
+setup(
+    name='whatTheFrame',
+    version='0.1.0',
+    packages=['whatTheFrame'],
+    package_dir={'whatTheFrame': 'src/whatTheFrame'}
+)
